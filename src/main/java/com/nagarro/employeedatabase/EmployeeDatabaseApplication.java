3830faf588b75ff4c5540d82dc25193979fc82e8
Sahil Srivastava <sahil.srivastava@nagarro.com>
2021-05-20 11:34:30 +0530

    add

--- src/main/java/com/nagarro/employeedatabase/EmployeeDatabaseApplication.java
+++ src/main/java/com/nagarro/employeedatabase/EmployeeDatabaseApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeDatabaseApplication {
-
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(EmployeeDatabaseApplication.class, args);
     }
-
 }
